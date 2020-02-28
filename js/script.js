dfc787b26e4ed1defeafb5a7d3d19788f5d42708
soumayakam <61125389+soumayakam@users.noreply.github.com>
2020-02-28 16:31:10 +0100

    merci valentin

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {  
+
+    // OWL-CAROUSEL
+    $("#owl-formation").owlCarousel({
+      pagination: false,
+      loop:true,
+      autoplay:true,
+      dots: false,
+      items: 2,
+      nav : true,
+      slideBy: 2,
+      autoplayTimeout:10000,
+      rewindSpeed: 1000
+    });
+
+
+});
